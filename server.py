90efc8e5f659a20151a8d4a04f19ee6694e468e9
Chris Thomas <faore@onhaven.net>
2016-09-18 00:37:23 -0600

    Server changed

--- server.py
+++ server.py
@@ -11,7 +11,17 @@ board = argv[2]
 
 f = open(board)
 
+cf = open('clientData', 'w+')
+cf.write(port)
+cf.close()
+
 grid = []
+
+oppGrid = []
+for i in range(0, 10):
+    oppGrid.append([])
+    for j in range(0, 10):
+        oppGrid[i].append('_')
 
 for i in range(0, 10):
     line = f.readline().strip('\n')
@@ -70,5 +80,57 @@ def show_board():
     content += str("</table>")
     return content
 
+@app.post('/self')
+def update_opponent():
+    # Check if x and y exist
+    try:
+        x = request.POST['x']
+        y = request.POST['y']
+        hit = request.POST['hit']
+    except KeyError:
+        response.status = 400
+        return
+    # Check if x is an int
+    try:
+        x = int(x)
+        y = int(y)
+        hit = int(hit)
+    except ValueError:
+        response.status = 400
+        return
+    # Check if x is out of bounds
+    if x >= 10 or y >= 10:
+        response.status = 404
+        return
+    # Check the coordinate
+    if hit == 1:
+        oppGrid[y][x] = 'O'
+    else:
+        oppGrid[y][x] = 'X'
 
-run(app, host='0.0.0.0', port=port, reloader=True)
+    hitcount = 0
+    # Check for a win
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[j][i] == 'O':
+                hitcount += 1
+
+    if hitcount == 17:
+        return parse.urlencode({'win': 1})
+    else:
+        return parse.urlencode({'win': 1})
+
+
+@app.get('/opponent_board.html')
+def opponent_board():
+    content = "<h1>Opponent Board Contents</h1><table>"
+    for i in range(0, len(oppGrid)):
+        content += str("<tr>")
+        for j in range(0, len(oppGrid[i])):
+            content += str("<td>") + str(oppGrid[i][j]) + str("</td>")
+        content += str("</tr>")
+    content += str("</table>")
+    return content
+
+
+run(app, host='0.0.0.0', port=port)
